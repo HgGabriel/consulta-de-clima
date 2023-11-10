05724d48243e7b14b4f940cc18664fa4fb4459e9
Hiago Gabriel <hiago@gmail.com>
2023-11-10 14:06:37 -0300

    Exercício aprendendo fetch API com typescript

---
+++ js/index.js
@@ -0,0 +1,41 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+const form = document.querySelector("#search-form > form");
+const input = document.querySelector("#input-localizacao");
+const sectionTempoInfos = document.querySelector("#tempo-info");
+form === null || form === void 0 ? void 0 : form.addEventListener('submit', (event) => __awaiter(void 0, void 0, void 0, function* () {
+    event.preventDefault();
+    if (!input || !sectionTempoInfos)
+        return;
+    let localizacao = input.value;
+    if (localizacao.length < 3) {
+        alert('Local não existe');
+        return;
+    }
+    const resposta = yield fetch(`https://api.openweathermap.org/data/2.5/weather?q=${localizacao}&appid=8133cc17ca8af17c6334eaf0d140916e&lang=pt_br&units=metric`);
+    const dados = yield resposta.json();
+    const infos = {
+        temperatura: Math.round(dados.main.temp),
+        local: dados.name,
+        icone: `https://openweathermap.org/img/wn/${dados.weather[0].icon}@2x.png`
+    };
+    sectionTempoInfos.innerHTML = `
+<div class="tempo-dados">
+        <h2>${infos.local}</h2>
+  
+        <span>${infos.temperatura}°</span>
+      </div>
+
+      <img src="${infos.icone}">
+      `;
+    console.log(dados);
+    input.value = '';
+}));
---
+++ ts/index.ts
@@ -0,0 +1,42 @@
+const form = document.querySelector("#search-form > form")
+const input: HTMLInputElement | null = document.querySelector("#input-localizacao")
+const sectionTempoInfos = document.querySelector("#tempo-info")
+
+form?.addEventListener('submit', async (event) => {
+  event.preventDefault()
+
+  if (!input||!sectionTempoInfos) return
+
+
+  
+  let localizacao = input.value
+
+  if(localizacao.length < 3) {
+    alert('Local não existe')
+    return
+  }
+
+const resposta = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${localizacao}&appid=8133cc17ca8af17c6334eaf0d140916e&lang=pt_br&units=metric`)
+
+const dados = await resposta.json()
+
+const infos = {
+  temperatura: Math.round(dados.main.temp),
+  local: dados.name,
+  icone: `https://openweathermap.org/img/wn/${dados.weather[0].icon}@2x.png`
+}
+
+sectionTempoInfos.innerHTML = `
+<div class="tempo-dados">
+        <h2>${infos.local}</h2>
+  
+        <span>${infos.temperatura}°</span>
+      </div>
+
+      <img src="${infos.icone}">
+      `
+
+console.log(dados)
+
+  input.value = ''
+})
